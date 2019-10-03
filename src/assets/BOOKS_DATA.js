8e678b0d52300f74086ebd312377908bc6dac7fa
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-03 16:35:09 +0200

    3. data for category & books

---
+++ src/assets/BOOKS_DATA.js
@@ -0,0 +1,45 @@
+const BOOKS_DATA = [
+  {
+    id: 10,
+    title: "ONE",
+    info: "something",
+    category: "action"
+  },
+  {
+    id: 11,
+    title: "Two",
+    info: "something",
+    category: "action"
+  },
+  {
+    id: 12,
+    title: "THREE",
+    info: "something",
+    category: "action"
+  },
+  {
+    id: 13,
+    title: "Four",
+    info: "something",
+    category: "history"
+  },
+  {
+    id: 14,
+    title: "five",
+    info: "something",
+    category: "action"
+  },
+  {
+    id: 15,
+    title: "six",
+    info: "something",
+    category: "romance"
+  },
+  {
+    id: 16,
+    title: "seven",
+    info: "something",
+    category: "action"
+  }
+];
+export default BOOKS_DATA;
---
+++ src/assets/CATEGORY_DATA.js
@@ -0,0 +1,34 @@
+const CATEGORY_DATA = [
+  {
+    id: 1,
+    category: "action",
+    info: "SOMETHING ABOUT ACTION HEROES"
+  },
+  {
+    id: 2,
+    category: "romance",
+    info: "Romantic novels"
+  },
+  {
+    id: 3,
+    category: "thriller",
+    info: "Bla Bla bla"
+  },
+  {
+    id: 4,
+    category: "comedy",
+    info: "HILARIOUS"
+  },
+  {
+    id: 5,
+    category: "mistery",
+    info: "GUESS WHO IS THIS"
+  },
+  {
+    id: 6,
+    category: "history",
+    info: "ANCIENT ROME"
+  }
+];
+
+export default CATEGORY_DATA;
