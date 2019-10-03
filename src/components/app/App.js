b4985197d54d4ec146965f40f386b7d62c0f772b
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-03 15:33:14 +0200

    2.8 added all components to App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
 import Homepage from "../../pages/homepage/homepage";
 import Header from "../header/header";
 import Footer from "../footer/footer";
+
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+        </Switch>
         <Footer />
       </div>
     );
