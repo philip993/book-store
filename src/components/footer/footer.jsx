38dc191553b9b04ca72ebb68dbc5b1657f394958
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-03 15:31:46 +0200

    2.5 created basic footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-contact">
+          <p>Mail@mail.com</p>
+          <p>Phone: 123-123-123</p>
+        </div>
+        <div className="footer-info">
+          <p>Copyrights 2019</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
