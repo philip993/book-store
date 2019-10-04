03e754e6346109f970ed71607408fb07b94cc5d3
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 17:17:11 +0200

    62. small changes

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import TrendingItems from "../../components/trending/trending-item";
 import CategoryItems from "../../components/category-items/category-items";
 import Category from "../../components/category/category";
+import OnSaleItem from "../../components/on-sales/on-sales-item";
+
+import "./homepage.scss";
 
 class Homepage extends React.Component {
   render() {
     return (
       <div className="homepage">
-        <TrendingItems />
+        <div className="home-items">
+          <TrendingItems />
+          <OnSaleItem />
+        </div>
+
         <CategoryItems />
         <Category />
       </div>
