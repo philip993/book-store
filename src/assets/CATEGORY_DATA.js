67ca5eb99f2fc5f66fcfbb441c6a2365a51a63a3
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 15:32:23 +0200

    4. updated data with URL

--- src/assets/CATEGORY_DATA.js
+++ src/assets/CATEGORY_DATA.js
@@ -2,32 +2,38 @@ const CATEGORY_DATA = [
   {
     id: 1,
     category: "action",
-    info: "SOMETHING ABOUT ACTION HEROES"
+    info: "SOMETHING ABOUT ACTION HEROES",
+    urlPath: "/shop/action"
   },
   {
     id: 2,
     category: "romance",
-    info: "Romantic novels"
+    info: "Romantic novels",
+    urlPath: "/shop/romance"
   },
   {
     id: 3,
     category: "thriller",
-    info: "Bla Bla bla"
+    info: "Bla Bla bla",
+    urlPath: "/shop/thriller"
   },
   {
     id: 4,
     category: "comedy",
-    info: "HILARIOUS"
+    info: "HILARIOUS",
+    urlPath: "/shop/comedy"
   },
   {
     id: 5,
     category: "mistery",
-    info: "GUESS WHO IS THIS"
+    info: "GUESS WHO IS THIS",
+    urlPath: "/shop/mistery"
   },
   {
     id: 6,
     category: "history",
-    info: "ANCIENT ROME"
+    info: "ANCIENT ROME",
+    urlPath: "/shop/history"
   }
 ];
 
