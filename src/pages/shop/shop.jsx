ffc3a9615fdda5a61c70437975d6f8a86d371a1d
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 15:37:51 +0200

    46. reworked shop components

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import BOOKS_DATA from "../../assets/BOOKS_DATA";
-import Book from "../../components/book/book";
+
+import BookCollection from "../../components/book-collection/book-collection";
+
+import "./shop.scss";
+
 class Shop extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      books: BOOKS_DATA
-    };
-  }
-
   render() {
-    const { books } = this.state;
     return (
       <div className="shop">
-        {books.map(({ id, ...otherProps }) => (
-          <Book key={id} {...otherProps} />
-        ))}
+        <BookCollection />
       </div>
     );
   }
