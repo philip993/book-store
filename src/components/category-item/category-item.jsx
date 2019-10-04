132ba6be5686c08ee6d8ae95d8d0d8e1d4a708cf
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 15:34:31 +0200

    44. Reworked category-item component

--- src/components/category-item/category-item.jsx
+++ src/components/category-item/category-item.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-const CategoryItem = ({ category, info }) => (
+import "./category-item.scss";
+
+const CategoryItem = book => (
   <div className="category-item">
-    <h1 className="category-title">{category}</h1>
-    <span>{info}</span>
+    <h2 className="c-title">{book.category.toUpperCase()}</h2>
+    <span className="c-info">{book.info.toLowerCase()}</span>
+    <button
+      className="c-button"
+      onClick={() => book.history.push(book.urlPath)}
+    >
+      <p>Go to {book.category.toUpperCase()} category.</p>
+    </button>
   </div>
 );
 
-export default CategoryItem;
+export default withRouter(CategoryItem);
