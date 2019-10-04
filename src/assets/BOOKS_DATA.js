f6d33181a68108a2acca287cd14b84dd3fa7a2eb
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 16:19:26 +0200

    50. updated books data with isTrending property

--- src/assets/BOOKS_DATA.js
+++ src/assets/BOOKS_DATA.js
@@ -3,43 +3,50 @@ const BOOKS_DATA = [
     id: 10,
     title: "ONE",
     info: "something",
-    category: "action"
+    category: "action",
+    isTrending: false
   },
   {
     id: 11,
     title: "Two",
     info: "something",
-    category: "action"
+    category: "action",
+    isTrending: false
   },
   {
     id: 12,
     title: "THREE",
     info: "something",
-    category: "action"
+    category: "action",
+    isTrending: false
   },
   {
     id: 13,
     title: "Four",
     info: "something",
-    category: "history"
+    category: "history",
+    isTrending: true
   },
   {
     id: 14,
     title: "five",
     info: "something",
-    category: "action"
+    category: "action",
+    isTrending: false
   },
   {
     id: 15,
     title: "six",
     info: "something",
-    category: "romance"
+    category: "romance",
+    isTrending: false
   },
   {
     id: 16,
     title: "seven",
     info: "something",
-    category: "action"
+    category: "action",
+    isTrending: true
   }
 ];
 export default BOOKS_DATA;
