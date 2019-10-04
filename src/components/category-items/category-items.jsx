d6950978ff708d910edcc4abb79bac5b71858a64
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 15:34:54 +0200

    45. changes in links

--- src/components/category-items/category-items.jsx
+++ src/components/category-items/category-items.jsx
@@ -9,22 +9,22 @@ class CategoryItems extends React.Component {
       <div className="category">
         <h3 className="category-title">Browse by Category:</h3>
         <div className="category-items">
-          <Link to="/category/action" className="c-link">
+          <Link to="/shop/action" className="c-link">
             Action
           </Link>
-          <Link to="/category/romance" className="c-link">
+          <Link to="/shop/romance" className="c-link">
             Romace
           </Link>
-          <Link to="/category/thriller" className="c-link">
+          <Link to="/shop/thriller" className="c-link">
             Thriller
           </Link>
-          <Link to="/category/comedy" className="c-link">
+          <Link to="/shop/comedy" className="c-link">
             Comedy
           </Link>
-          <Link to="/category/mistery" className="c-link">
+          <Link to="/shop/mistery" className="c-link">
             Mistery
           </Link>
-          <Link to="category/history" className="c-link">
+          <Link to="shop/history" className="c-link">
             History
           </Link>
         </div>
