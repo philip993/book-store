2fb8c9925f38e7fd3f07441dc006af1b0889477e
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 16:58:46 +0200

    54. added price to book component

--- src/components/book/book.jsx
+++ src/components/book/book.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./book.scss";
 
-const Book = ({ category, title, info }) => (
+const Book = ({ category, title, info, price }) => (
   <div className="book">
     <h3 className="book-title">{title}</h3>
     <span className="book-info">{info}</span>
+    <br />
+    <span>${price}</span>
     <p className="book-category">{category}</p>
   </div>
 );
