3145afe434d663824f2c356809e5b4fe5d56a8af
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-04 15:33:37 +0200

    42. Book-collection components with all items

---
+++ src/components/book-collection/book-collection.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BOOKS_DATA from "../../assets/BOOKS_DATA";
+import Book from "../book/book";
+
+class BookCollection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: BOOKS_DATA
+    };
+  }
+
+  render() {
+    const { sections } = this.state;
+    return (
+      <div>
+        {sections.map(({ id, ...otherProps }) => (
+          <Book key={id} {...otherProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default BookCollection;
