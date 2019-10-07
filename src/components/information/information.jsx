eeb91740a3a123837a51f773c6312c81ea49b644
Filip Mihajlovski <fitz993m@gmail.com>
2019-10-07 13:46:32 +0200

    71. added information component

---
+++ src/components/information/information.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Information = () => (
+  <div className="information">
+    <h2>System Information!</h2>
+    <span>There aren't any books in this category yet!</span>
+  </div>
+);
+
+export default Information;
